f0a2f118689c8d76ccb496a1d473b2637b42f611
Jeff Heaton <jeff@jgheaton.com>
2019-02-20 10:06:04 -0800

    Update and add tests

--- jheader.py
+++ jheader.py
@@ -1,71 +1,85 @@
 #!/usr/bin/python
 # encoding: utf-8
-import sys
 import datetime as dt
-from workflow import Workflow3
+import sys
+from unittest import TestCase
+
 from dateutil.relativedelta import *
+from freezegun import freeze_time
+from isoweek import Week
+from workflow import Workflow3
 
 log = None
+
 
 # goal
 # # 29 Summer ☀ w5:13 ㄖ w6:52
 # ▓▓▓▓▓▓▓▓░░░░░░░░░░░░░░░░░░ /6 weeks remaining/
 # Using the ISO 8601 calendar and UTC time, should I use EST/Sunday instead?
 
-dob = dt.datetime(1989, 5, 28)
-now = dt.datetime.utcnow()
-birthday = dt.datetime(now.year, 5, 28)
-lastYear = now.year - 1
-endOfLastYear = dt.datetime(lastYear, 12, 31)
-age = relativedelta(now, dob).years
+class GenerateHeaderTests(TestCase):
+    @freeze_time('2001-03-01')
+    def test_should_generate_header(self):
+        actual = main()
+        expected = ''
+        self.assertEqual(actual, expected)
 
-if now < birthday:
-    myWeekNumber = (endOfLastYear.isocalendar()[1] - dt.datetime(lastYear, 5, 28).isocalendar()[1])
-else:
-    myWeekNumber = now.isocalendar()[1] - birthday.isocalendar()[1]
+    @freeze_time('2001-01-01')
+    def test_should_handle_winter_weeks(self):
+        pass
 
-# current season and weeks in integers
-if 3 <= now.month <= 5:
-    season = 'Spring ❀'
-    seasonWeekNumber = int(now.isocalendar()[1]) - int(dt.datetime(now.year, 3, 1).isocalendar()[1])
-elif 8 >= now.month >= 6:
-    season = 'Summer ☀'
-    seasonWeekNumber = int(now.isocalendar()[1]) - int(dt.datetime(now.year, 6, 1).isocalendar()[1])
-elif 11 >= now.month >= 9:
-    season = 'Fall ☕︎'
-    seasonWeekNumber = int(now.isocalendar()[1]) - int(dt.datetime(now.year, 9, 1).isocalendar()[1])
-else:
-    season = 'Winter ❄'
-    lastYear = now.year - 1
-    
-    if int(now.month) == 12:
-        lastYearsWeeks = 0
-        seasonStart = int(dt.datetime(lastYear, 12, 1).isocalendar()[1])
+
+def generate_header():
+    dob = dt.datetime(1989, 5, 28, tzinfo=dt.timezone.utc)
+    now = dt.datetime.now(dt.timezone.utc)
+    last_year = now.year - 1
+    last_week_of_last_year = Week.last_week_of_year(last_year)
+    age = relativedelta(now, dob).years
+    my_birthday_happened_this_year = now < dt.datetime(now.year, 5, 28, tzinfo=dt.datetime.utc)
+
+    if my_birthday_happened_this_year:
+        last_birthday = dt.datetime(now.year, 5, 28)
+        weeks_lived_this_year = Week.thisweek() - Week.withdate(last_birthday)
     else:
-        lastYearsWeeks = int(endOfLastYear.isocalendar()[1]) - int(dt.datetime(lastYear, 12, 1).isocalendar()[1])
-        seasonStart = int(dt.datetime(now.year, 1, 1).isocalendar()[1])
+        last_birthday = dt.datetime(last_year, 5, 28)
+        weeks_lived_this_year = Week(now) + last_week_of_last_year - Week.withdate(last_birthday)
 
-    seasonWeekNumber = int(now.isocalendar()[1]) - seasonStart - lastYearsWeeks
+    seasons = [([3, 4, 5], 'Spring ❀'), ([6, 7, 8], 'Summer ☀'), ([9, 10, 11], 'Fall ☕︎'), ([12, 1, 2], 'Winter ❄'), ]
 
-# seasonal loader
-empty = '░'
-emptyAmount = empty * (13 - (seasonWeekNumber - 1)) * 2
-filled = '▓'
-filledAmount = filled * (seasonWeekNumber - 1) * 2
-# Subtracting one because I generate these at the beginning of the week
-loader = "{0}{1}".format(filledAmount, emptyAmount)
-# percentage remaining
-# percent = int((float(13 - (seasonWeekNumber - 1))/13)*100)
-# percent = float(13 - (seasonWeekNumber - 1)) / 13
-# percentRemaining = "/ {}% remaining /".format(percent)
-weeksRemaining = "/ {} weeks remaining /".format(13-seasonWeekNumber)
+    season_week_number = 0
+
+    for season in seasons:
+        if now.month in season[0]:
+            current_season = season[1]
+            season_week_number = Week.thisweek() - Week.withdate(
+                dt.datetime(now.year, season[0][0], 1, tzinfo=dt.timezone.utc))
+
+        if now.month in [1, 2]:
+            season_week_number += last_week_of_last_year - Week.withdate(
+                dt.datetime(last_year, 12, 1, tzinfo=dt.timezone.utc))
+
+    assert season_week_number != 0
+
+    # seasonal loader
+    empty = '░'
+    emptyAmount = empty * (13 - (season_week_number - 1)) * 2
+    filled = '▓'
+    filled_amount = filled * (season_week_number - 1) * 2
+    # Subtracting one because I generate these at the beginning of the week
+    loader = "{0}{1}".format(filled_amount, emptyAmount)
+    # percentage remaining
+    # percent = int((float(13 - (seasonWeekNumber - 1))/13)*100)
+    # percent = float(13 - (seasonWeekNumber - 1)) / 13
+    # percentRemaining = "/ {}% remaining /".format(percent)
+    weeks_remaining = "/ {} weeks remaining /".format(13 - season_week_number)
+
+    return "# {0} {1} {2}:13 ㄖ {3}:52\n{4} {5}".format(age, season, season_week_number, weeks_lived_this_year, loader,
+                                                         weeks_remaining)
 
 
 def main(wf):
-    header = "# {0} {1} {2}:13 ㄖ {3}:52\n{4} {5}".format(age, season, seasonWeekNumber, myWeekNumber, loader,
-                                                           weeksRemaining)
-    now_string = now.isoformat()
-    wf.add_item(title=header, subtitle=now_string, arg=header, valid=True)
+    now_string = dt.datetime.now(dt.timezone.utc).isoformat()
+    wf.add_item(title=generate_header(), subtitle=now_string, arg=generate_header(), valid=True)
     wf.send_feedback()
 
 
